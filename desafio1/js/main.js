1ec1a7b30027542682562d707d9311b42df49dab
juani <juani.trotti@gmail.com>
2021-12-20 21:11:14 -0300

    desafio1

---
+++ desafio1/js/main.js
@@ -0,0 +1,8 @@
+// variables
+var usuario = prompt("Ingrese su usuario");
+var fechaNa = parseInt(prompt("Ingrese fecha de nacimiento"));
+var edad = (2021 - fechaNa);
+
+//imprimir consola
+console.log("Tu numbre es " + usuario + " y tu edad es " + edad);
+alert("Tu numbre es " + usuario + " y tu edad es " + edad);
