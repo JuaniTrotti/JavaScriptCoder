e7dcf97b8503cf5aa32953caa230f326f19df1ec
juani <juani.trotti@gmail.com>
2022-01-24 21:48:22 -0300

    generarHTML

---
+++ desafiosComplementarios/generarHTML/js/main.js
@@ -0,0 +1,26 @@
+// clases
+class personas {
+    constructor(n1, n2) {
+        this.nombre = n1.toLowerCase();
+        this.edad = parseInt(n2)
+    }
+}
+
+// variables
+const arrayPersonas = [];
+
+// cargo el array
+arrayPersonas.push(new personas("Oscar", "43"));
+arrayPersonas.push(new personas("Marcela", "43"));
+arrayPersonas.push(new personas("Mercedes", "42"));
+arrayPersonas.push(new personas("Palacios", "22"));
+
+// muestro el array original
+console.log(arrayPersonas);
+
+// ordeno el array de menor a mayor
+arrayPersonas.sort(function(a,b){return a.edad - b.edad;});
+
+// muestro el array ordenado
+console.log(arrayPersonas.sort());
+
