093a3dac81783cce9323d2295da4983fc48619db
juani <juani.trotti@gmail.com>
2022-01-28 22:35:25 -0300

    proyecto final js

--- Proyectofinal/ProyectoFinal/js/card.js
+++ Proyectofinal/ProyectoFinal/js/card.js
@@ -1,4 +1,4 @@
-class card{
+class card {
     constructor(id, nombre, precio, descripcion) {
         this.id = parseInt(id);
         this.nombre = nombre.toLowerCase();
@@ -7,20 +7,4 @@ class card{
     }
 }
 
-const cards = [];
-
-cards.push(new card(1, "obra1", 230, "desc obra1"));
-cards.push(new card(2, "obra2", 530, "desc obra2"));
-cards.push(new card(3, "obra3", 830, "desc obra3"));
-cards.push(new card(4, "obra4", 130, "desc obra4"));
-
-
-
-for(let i of cards) {
-    let section = document.getElementById("containerO");
-    let div = document.createElement("div");
-    div.innerHTML = `<a href="index2.html"><div class="obra cFlex"><h4>${i.nombre}</h4><h5>${i.precio}</h5></div></a>`;
-    section.appendChild(div);
-}
-
-// export default card;
+export default card;
--- Proyectofinal/ProyectoFinal/js/main.js
+++ Proyectofinal/ProyectoFinal/js/main.js
@@ -1,7 +1,20 @@
 // import de clases
+import card from "./card.js";
 
 //variables
+const obras = [];
 
 // funciones
 
 //main
+obras.push(new card(1, "obra1", 230, "desc obra1"));
+obras.push(new card(2, "obra2", 530, "desc obra2"));
+obras.push(new card(3, "obra3", 830, "desc obra3"));
+obras.push(new card(4, "obra4", 130, "desc obra4"));
+
+for(let i of obras) {
+    let section = document.getElementById("containerO");
+    let div = document.createElement("div");
+    div.innerHTML = `<a href="index2.html"><div class="obra cFlex"><h4>${i.nombre}</h4><h5>${i.precio}</h5></div></a>`;
+    section.appendChild(div);
+}
